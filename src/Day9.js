e7216a76ae9ecc5fe435002b12f55c9289b0f788
nustonic <nustjrx@gmail.com>
2024-09-13 10:34:51 +0700

    second update
    u2

--- src/Day9.js
+++ src/Day9.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const API_URL = "https://sample-api-fwbm.onrender.com/api/v1";
-
+const CRYPTO_API = "https://api.coingecko.com/api/v3/coins/markets";
 const ImageCarousel = ({ images }) => {
     const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -16,13 +16,13 @@ const ImageCarousel = ({ images }) => {
 
     return (
         <div className="carousel">
-           
+
             <button className="carousel-btn prev" onClick={prevSlide}>&#10094;</button>
             <div className="carousel-images">
                 {images.map((image, index) => (
                     <img
-                        src={image} 
-                        key={index} 
+                        src={image}
+                        key={index}
                         alt={`Slide ${index + 1}`}
                         className={`carousel-image ${index === currentIndex ? 'active' : ''}`}
                     />
@@ -36,22 +36,45 @@ const ImageCarousel = ({ images }) => {
 
 const Day9 = () => {
     const carouselImages = [
-        'https://cdn.mycplus.com/mycplus/wp-content/uploads/2020/02/programming_tips.jpg',
-        'https://www.stellardigital.in/blog/wp-content/uploads/2022/03/What-skills-are-required-to-become-an-efficient-coder-min.jpg',
+
         'https://eduauraapublic.s3.ap-south-1.amazonaws.com/webassets/images/blogs/how-to-improve-coding-skills.jpg',
-        'https://helios-i.mashable.com/imagery/articles/01gIa563Hg9IvYRYIP64WQd/hero-image.fill.size_1248x702.v1656434265.jpg'
+        'https://www.stellardigital.in/blog/wp-content/uploads/2022/03/What-skills-are-required-to-become-an-efficient-coder-min.jpg',
+        'https://helios-i.mashable.com/imagery/articles/01gIa563Hg9IvYRYIP64WQd/hero-image.fill.size_1248x702.v1656434265.jpg',
+        'https://cdn.mycplus.com/mycplus/wp-content/uploads/2020/02/programming_tips.jpg',
     ];
+    const [cryptoData, setCryptoData] = useState([]);
+    const fetchCryptoData = async () => {
+        try {
+            const res = await axios.get(CRYPTO_API, {
+                params: {
+                    vs_currency: 'usd',
+                    //dont use ids if want to show top coin, use it when you want to show some coins
+                    ids: 'bitcoin,gala,ethereum,binancecoin',
+                    order: 'market_cap_desc',
+                    per_page: 5,
+                    page: 1,
+                    sparkline: false
+                }
+            });
+            setCryptoData(res.data);
+        } catch (error) {
+            console.error("Error fetching crypto data", error);
+        }
+    };
 
-
-   
     const [user, setUser] = useState(null);
 
-   
 
     const navigate = useNavigate();
     useEffect(() => {
+        if (!localStorage.getItem('token')) {
+            navigate('/auth')
+        }
+    }, [])
+    useEffect(() => {
 
         fecthMyUser();
+        fetchCryptoData();
     }, []
     )
     const fecthMyUser = async () => {
@@ -65,7 +88,7 @@ const Day9 = () => {
     }
 
 
-   
+
 
     return (
 
@@ -74,7 +97,7 @@ const Day9 = () => {
             <h1>Profile Information</h1>
             <div className="container">
                 <div className="left-column">
-                    <img className="profile-image" width={200} height={200} src="/IMG_3413.jpg"></img>
+                    <img className="profile-image" width={200} height={200} src="/photo.png"></img>
                     <div className="info">
                         <center><h2> {user?.first_name} {user?.surname}</h2></center>
                         <p><strong>Email:</strong> {user?.email}</p>
@@ -90,7 +113,7 @@ const Day9 = () => {
                         <img width={50} height={50} src="https://images.vexels.com/media/users/3/223136/isolated/preview/984f500cf9de4519b02b354346eb72e0-facebook-icon-social-media-by-vexels.png"></img>
                         <p><a href="http://www.facebook.com/nustino"> Ntesla</a></p>
                         <img width={50} height={50} src="https://static.vecteezy.com/system/resources/previews/016/716/450/original/tiktok-icon-free-png.png"></img>
-                        <p id='tk'> Time Traveller</p>
+                        <p id='tk'><a href="http://www.tiktok.com/@cpnust"> Time Travel</a></p>
                     </div>
 
                     <div className="real">
@@ -115,6 +138,20 @@ const Day9 = () => {
                         <img className="qr" height={200} width={200} src="https://images.squarespace-cdn.com/content/v1/6059e881e22c8042fb31c88f/e3a9de00-c0b7-4c92-81ce-6a9f33f95148/Screenshot+2022-11-25+at+14.48.47.png"></img>
                         <ImageCarousel images={carouselImages} />
                     </div></div>
+                <div className="crypto-watchlist">
+                    <h2>Crypto Watchlist</h2>
+                    {cryptoData.map((coin) => (
+                        <div key={coin.id} className="crypto-row">
+                            <img className="crypto-icon" src={coin.image} alt={`${coin.name} icon`} />
+                            <span className="crypto-name">{coin.name}:</span>
+                            <span className="crypto-price">${new Intl.NumberFormat().format(coin.current_price)}</span>
+                            <span
+                                className={`crypto-change ${coin.price_change_percentage_24h >= 0 ? 'positive' : 'negative'}`}>
+                                {coin.price_change_percentage_24h.toFixed(2)}%
+                            </span>
+                        </div>
+                    ))}
+                </div>
             </div>
 
             <style jsx>{`
@@ -168,7 +205,7 @@ const Day9 = () => {
                         border: solid 5px white;
                     }
         
-                    .info, .info2, .real {
+                    .info, .info2, .real, .crypto-watchlist{
                         padding: 15px;
                         background-color: rgba(255, 255, 255, 0.9);
                         border-radius: 20px;
@@ -176,7 +213,59 @@ const Day9 = () => {
                         text-align: left;
                         
                     }
+                       .crypto-watchlist h2{
+                       color: #fc7f03;
+                       
 
+                       }
+                       .crypto-watchlist {
+                       background-color: #332f2b;}
+
+                 .crypto-watchlist {
+                    margin-top: 20px;
+                    padding: 10px;
+                    
+                }
+
+                .crypto-row {
+                    display: flex;
+                    justify-content: space-between;
+                    padding: 10px;
+                    background-color: rgba(255, 255, 255, 0.8);
+                    border-bottom: 1px solid rgba(0,0,0,0.1);
+                    border-radius: 2px;
+                }
+
+                .crypto-icon {
+                    width: 30px;
+                    height: 30px;
+                    margin-right: 10px;
+                }
+
+                .crypto-name {
+                    font-weight: bold;
+                    flex: 1;
+                }
+
+                .crypto-price {
+                    margin-left: 10px;
+                    flex: 1;
+                }
+
+                .crypto-change {
+                    margin-left: auto;
+                    font-weight: bold;
+                    flex: 1;
+                    text-align: right;
+                }
+
+                .crypto-change.positive {
+                    color: green;
+                }
+
+                .crypto-change.negative {
+                    color: red;
+                }
                     .real h2 {
                     color:red;
                     }
